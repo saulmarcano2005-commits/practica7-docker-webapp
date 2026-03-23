bad7257934b2ba5f69035e63dc6368fcb0aa2de4
Saul Marcano <saulmarcano2005@gmail.com>
2026-03-22 21:51:48 -0400

    Update app.py
    update app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import mysql.connector
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hola Mundo Docker + MySQL"
+    return "Hola Mundo Docker + MySQL funcionando"
 
-app.run(host='0.0.0.0',port=5000)from flask import Flask
-
+app.run(host='0.0.0.0',port=5000)
